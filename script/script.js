71e08141cde9d2702ff1d52a405063b8eb585a5f
Mamali <mohammadrezaghorbani997@gmail.com>
2025-10-07 10:37:11 +0330

    1072025

---
+++ script/script.js
@@ -0,0 +1,47 @@
+const textElement = document.getElementById('animated-text');
+const texts = [
+    "ساخت سایت با طراحی دلخواه شما",
+    "سایت ریسپانسیو",
+    "قیمت مناسب",
+    "تضمین کیفیت"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let typingSpeed = 100;
+const pauseTime = 4000; // 10 ثانیه وقفه
+
+function typeWriter() {
+    const currentText = texts[textIndex];
+
+    if (isDeleting) {
+        // حالت پاک کردن
+        textElement.innerHTML = currentText.substring(0, charIndex - 1) + '<span class="cursor">|</span>';
+        charIndex--;
+
+        if (charIndex === 0) {
+            isDeleting = false;
+            textIndex = (textIndex + 1) % texts.length;
+            typingSpeed = 100;
+        }
+        setTimeout(typeWriter, 50);
+    } else {
+        // حالت تایپ کردن
+        textElement.innerHTML = currentText.substring(0, charIndex + 1) + '<span class="cursor">|</span>';
+        charIndex++;
+
+        if (charIndex === currentText.length) {
+            // وقتی نوشتن تمام شد، 10 ثانیه صبر کن سپس پاک کردن را شروع کن
+            isDeleting = true;
+            setTimeout(typeWriter, pauseTime);
+            return;
+        }
+        setTimeout(typeWriter, typingSpeed);
+    }
+}
+
+// شروع انیمیشن
+typeWriter();
+AOS.init();
+particlesJS("particles-js", { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true }); var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function () { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
